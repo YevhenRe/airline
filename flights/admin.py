2942a1d5af2e7997cbc4d21382d24ce95f81e77e
Yevhen Repiakh <erepyah@gmail.com>
2023-08-21 12:22:14 +0300

    oll last and use wrong valid flight function

--- flights/admin.py
+++ flights/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import Flight, Airport, Passanger
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "distination", "duration")
+
+class PassengersAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passanger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passanger, PassengersAdmin)
 
